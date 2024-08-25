384159e1d39df0f888598c6d8ab27ba0c39a0aa6
64160175 <64160175@go.buu.ac.th>
2024-08-26 04:58:53 +0700

    commit 26/08/2567 Login Update, DB Update

--- models/loginModels.js
+++ models/loginModels.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql'); 
+const bcrypt = require('bcrypt');
 let connection; // Store the database connection
 
 
@@ -18,19 +19,35 @@ exports.login = (username, password, callback) => {
 
   // SQL query to find the user (assuming you're hashing passwords)
   // Added "AND u_status = 1" to check for active users
-  const sql = 'SELECT * FROM tbl_user WHERE u_name = ? AND u_pass = SHA1(?) AND u_status = 1'; 
+  const sql = 'SELECT * FROM tbl_user WHERE u_name = ? AND u_status = 1';  
+  
 
-  connection.query(sql, [username, password], (err, results) => {
+  connection.query(sql, [username], (err, results) => {
     if (err) {
       console.error('Error during login:', err);
       return callback(err, null); 
     }
 
     if (results.length === 1) {
-      // User found and u_status is 1 (active)
-      return callback(null, results[0]); // Return the user object
+      const user = results[0];
+
+      // Use bcrypt.compare() to compare passwords
+      bcrypt.compare(password, user.u_pass, (err, match) => {
+        if (err) {
+          console.error('Error comparing passwords:', err);
+          return callback(new Error('Error during login'), null);
+        }
+
+        if (match) {
+          // Passwords match, login successful
+          return callback(null, user); 
+        } else {
+          // Passwords don't match
+          return callback(new Error('Invalid username or password'), null); 
+        }
+      });
+
     } else {
-      // User not found or u_status is not 1 (inactive)
       return callback(new Error('Invalid username or password'), null); 
     }
   });
