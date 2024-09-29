193692ca1a92e792026d51d4a7a2eef131371439
Kitsana Thongbunluea <64160175@go.buu.ac.th>
2024-09-29 23:21:04 +0700

    Dashboard show

--- public/js/today.js
+++ public/js/today.js
@@ -4,6 +4,3 @@ const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: false  };
 
 document.getElementById('currentDate').textContent = today.toLocaleDateString(undefined, dateOptions);
 document.getElementById('currentTime').textContent = today.toLocaleTimeString(undefined, timeOptions);
-
-//ใน dashboard_day
-document.getElementById('day_show').textContent = `วันนี้   ${today.toLocaleDateString('th-TH', dateOptions)}`;
