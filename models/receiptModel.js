86f8e3690324d0b34aa6b8055195985977727114
64160194 <64160194@go.buu.ac.th>
2024-10-13 01:42:55 +0700

    receipt 2nd

--- models/receiptModel.js
+++ models/receiptModel.js
@@ -3,8 +3,14 @@ const db = require('../config/database');
 const receiptModel = {
   getUsersForReceipt: (callback) => {
     const query = `
-      SELECT u.id, u.f_name, u.l_name, 
-             COALESCE(u.u_profile, d.u_profile) AS u_profile
+      SELECT 
+        u.id, 
+        u.f_name, 
+        u.l_name, 
+        u.u_name,
+        u.u_namebank, 
+        u.u_idbook,
+        COALESCE(u.u_profile, d.u_profile) AS u_profile
       FROM tbl_user u
       LEFT JOIN tbl_default_pic d ON 1=1
       WHERE u.u_type_name_id = 2 AND u.u_status = 1
