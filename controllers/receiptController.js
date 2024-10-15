47817c193950e557d09ccf1439d27f3f758d4424
64160194 <64160194@go.buu.ac.th>
2024-10-15 17:36:48 +0700

    receipt 3rd

--- controllers/receiptController.js
+++ controllers/receiptController.js
@@ -10,15 +10,21 @@ const receiptController = {
       const usersWithProfilePics = users.map(user => {
         if (user.u_profile) {
           if (Buffer.isBuffer(user.u_profile)) {
-            // ถ้าเป็น Buffer ให้แปลงเป็น Base64
             user.profile_pic = `data:image/jpeg;base64,${user.u_profile.toString('base64')}`;
           } else if (typeof user.u_profile === 'string') {
-            // ถ้าเป็น string (path) ให้ใช้โดยตรง
             user.profile_pic = user.u_profile;
           }
         } else {
-          user.profile_pic = '/image/profile.jpg'; // รูปเริ่มต้น
+          user.profile_pic = '/image/profile.jpg';
         }
+
+        // คำนวณชั่วโมงรวมจาก salary_data
+        user.total_hours = user.salary_data.reduce((sum, day) => sum + parseFloat(day.day_hr), 0);
+        user.total_hours = Math.round(user.total_hours * 100) / 100; // ปัดเศษทศนิยมให้เหลือ 2 ตำแหน่ง
+
+        //console.log(`User ${user.f_name} total hours: ${user.total_hours}`);
+        //console.log(`User ${user.f_name} salary data:`, JSON.stringify(user.salary_data, null, 2));
+
         return user;
       });
 
