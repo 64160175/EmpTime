81d8fe07bde51d391aaf3448b1eedbf50245d3fe
64160194 <64160194@go.buu.ac.th>
2024-10-19 22:13:12 +0700

    history hr

--- models/userWorkhistoryModel.js
+++ models/userWorkhistoryModel.js
@@ -3,28 +3,31 @@ const db = require('../config/database');
 const UserWorkhistoryModel = {
   getWorkHistory: (userId) => {
     return new Promise((resolve, reject) => {
-      const sql = `
+      const query = `
         SELECT 
-          ci.u_name AS user_id, 
+          ci.u_name AS user_id,
           DATE_FORMAT(ci.in_date, '%Y-%m-%d') AS work_date,
           ci.in_time AS actual_in_time,
-          co.out_time AS actual_out_time
+          co.out_time AS actual_out_time,
+          COALESCE(d.day_hr, 0) as day_hr
         FROM 
           tbl_checkin ci
         LEFT JOIN 
           tbl_checkout co ON ci.u_name = co.u_name AND DATE(ci.in_date) = DATE(co.out_date)
+        LEFT JOIN 
+          tbl_day_salary d ON ci.u_name = d.u_name AND DATE(ci.in_date) = d.work_date
         WHERE 
-          ci.u_name = (SELECT u_name FROM tbl_user WHERE id = ?) 
+          ci.u_name = (SELECT u_name FROM tbl_user WHERE id = ?)
         ORDER BY 
-          work_date DESC, ci.in_time DESC
+          ci.in_date DESC, ci.in_time DESC
       `;
 
-      db.query(sql, [userId], (err, results) => {
+      db.query(query, [userId], (err, results) => {
         if (err) {
           console.error('Error fetching work history:', err);
-          reject(err); 
+          reject(err);
         } else {
-          resolve(results); 
+          resolve(results);
         }
       });
     });
